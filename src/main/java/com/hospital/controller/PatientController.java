61e1f5c4d04de1d6e1958dce9bf7d2d4f239221e
Gopinath <gopin2221@gmail.com>
2024-12-24 14:52:35 +0530

    new commit

---
+++ src/main/java/com/hospital/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.hospital.controller;
+
+import com.hospital.entity.Patient;
+import com.hospital.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+@CrossOrigin(origins = "*")
+public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @PostMapping
+    public Patient savePatient(@RequestBody Patient patient) {
+        return patientService.savePatient(patient);
+    }
+}
+
---
+++ src/main/java/com/hospital/service/PatientService.java
@@ -0,0 +1,23 @@
+package com.hospital.service;
+
+import com.hospital.entity.Patient;
+import com.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+}
+
